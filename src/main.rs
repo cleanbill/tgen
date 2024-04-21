a738b48ec68b6838218dffe36eabcb729c0f2b2a
Mick Carter <cick.marter@gmail.com>
2024-04-21 16:54:43 +0100

    A bit of a clean

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ fn setup_args() -> (String, String) {
     } else {
         print!("Using {} ", args[1]);
     }
+    println!("");
 
     if qty_args_ok {
         return (args[1].to_string(), args[2].to_string());
@@ -21,7 +22,7 @@ fn setup_args() -> (String, String) {
     return ("./template.html".to_string(), "./locale-config.json".to_string());
 }
 
-fn populate(locale: String, config: &Value, template_filename: &str) {
+fn populate(index: i32, locale: String, config: &Value, template_filename: &str) {
     let mut template = fs
         ::read_to_string(template_filename)
         .expect("Should have been able to read template file");
@@ -29,7 +30,13 @@ fn populate(locale: String, config: &Value, template_filename: &str) {
     output_filename.push_str(&locale);
     output_filename.push_str(".html");
 
-    println!("Populating {} with {} to produce {} ", locale, template_filename, output_filename);
+    println!(
+        "{}. Populating {} with {} to produce {} ",
+        index,
+        locale,
+        template_filename,
+        output_filename
+    );
 
     let labels = config.get("labels").unwrap();
     let links = config.get("footerLinks").unwrap();
@@ -57,16 +64,17 @@ fn populate(locale: String, config: &Value, template_filename: &str) {
 fn main() {
     let args: (String, String) = setup_args();
     println!("The template file is {} and the json translation directory is {}", args.0, args.1);
-
+    println!("");
     let translations = fs
         ::read_to_string(args.1)
         .expect("Should have been able to read translation file");
     let json: serde_json::Value = serde_json
         ::from_str(translations.as_str())
         .expect("translation JSON was not well-formatted");
+    let mut index = 1;
     for key in json.as_object().unwrap().keys() {
-        println!("{}", key);
         let config: &Value = json.get(key).unwrap();
-        populate(key.to_string(), config, &args.0);
+        populate(index, key.to_string(), config, &args.0);
+        index = index + 1;
     }
 }
